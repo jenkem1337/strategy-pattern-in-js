da35abc0e4e25c78cdd7dc080987851bd03829a0
Can <78204287+jenkem1337@users.noreply.github.com>
2021-06-13 20:44:12 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,84 @@
+class LoginForm{
+    constructor(){
+        this.div = document.getElementById("formComesHere")
+    }
+    render(){
+        let template = `
+        <form>
+            <h1>Login Form</h1> <br>
+            <input type="text" placeholder="Nick Name"> <br>
+            <input type="password" placeholder="Password"> <br>
+            <button>Log in</button>
+        </form>
+
+        `
+        this.div.innerHTML = template
+    }
+}
+
+class RegisterForm{
+    constructor(){
+        this.div = document.getElementById("formComesHere")
+    }
+    render(){
+        let template = `
+        <form>
+            <h1>Register Form</h1> <br>
+            <input type="text" placeholder="Nick Name"> <br>
+            <input type="text" placeholder="Email"> <br>
+            <input type="password" placeholder="Password"> <br>
+            <button>Register</button>
+        </form>
+
+        `
+        this.div.innerHTML = template
+    }
+}
+
+class AdminForm{
+    constructor(){
+        this.div = document.getElementById("formComesHere")
+    }
+    render(){
+        let template = `
+        <form>
+            <h1>Admin Form</h1> <br>
+            <input type="text" placeholder="Admin Name"> <br>
+            <input type="password" placeholder="Password"> <br>
+            <button>Log in</button>
+        </form>
+        `
+        this.div.innerHTML = template
+    }
+}
+
+class FormContext{
+    constructor(form){
+        this.form = form
+    }
+
+    renderForm(){
+        this.form.render()
+    }
+
+}
+
+
+const loginBTN = document.getElementById("loginForm")
+const registerBTN = document.getElementById("registerForm")
+const adminBTN = document.getElementById("adminForm")
+
+loginBTN.addEventListener("click", ()=>{
+    let loginForm = new FormContext(new LoginForm())
+    loginForm.renderForm()
+})
+
+registerBTN.addEventListener("click", () =>{
+    let registerForm = new FormContext(new RegisterForm())
+    registerForm.renderForm()
+})
+
+adminBTN.addEventListener("click", ()=>{
+    let adminForm = new FormContext(new AdminForm())
+    adminForm.renderForm()
+})
---
+++ readme.md
@@ -0,0 +1,3 @@
+Hello everyone this is a basic example of strategy pattern in javascript.
+
+In this example we have three different button and that buttons rendering different form with strategy pattern
